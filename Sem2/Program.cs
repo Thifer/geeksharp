37cf8cc0e96a488eaa4d604faebe510172b1723c
NoName <buybackbrain.genetic@gmail.com>
2023-11-30 01:28:32 +0300

    fix location and project

---
+++ Sem2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Sem2;
+
+Collection<string> collection = new Collection<string>(10);
+
+collection[0] = "new string";
+collection[1] = "new string1";
+
+
+Bits bit = new(100);
+Bits bitInt = new Bits((int)10);
+Bits bitByte = new Bits((byte)11);
+Bits bitLong = new Bits((long)12);
+
+long _long = bitLong;
+int _int = bitInt;
+byte _byte = bitByte;
+
+
+Console.WriteLine(_long.GetType());
+Console.WriteLine(_long);
+Console.WriteLine(_int.GetType());
+Console.WriteLine(_int);
+Console.WriteLine(_byte.GetType());
+Console.WriteLine(_byte);
+
