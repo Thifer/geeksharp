06676620c6fea5ab8790efb8f98cda359d111f18
NoName <buybackbrain.genetic@gmail.com>
2023-12-01 21:28:35 +0300

    cleanup

--- Sem3/Program.cs
+++ Sem3/Program.cs
@@ -2,42 +2,6 @@
 
 using System.Collections;
 using Sem3;
-
-/*List<int> list = new (){1,2,3,4,5};
-Stack<int> stack = new (list);
-//Console.WriteLine(string.Join(" ",_list));
-list.Clear();
-while (stack.Count > 0)
-{
-    list.Add(stack.Pop());
-}
-//Console.WriteLine(string.Join("",_list));
-
-var customEnumerable = new CustomEnumerable();
-foreach (var x in customEnumerable)
-{
-    Console.WriteLine(x);
-}
-
-Console.WriteLine();
-foreach (var x in customEnumerable)
-{
-    Console.WriteLine(x);
-}
-*/
-
-
-/*Есть лабиринт описанный в виде двумерного массива где 1 это стены, 0 - проход, 2 - искомая цель.
-Пример лабиринта:
-1 1 1 1 1 1 1
-1 0 0 0 0 0 1
-1 0 1 1 1 0 1
-0 0 0 0 1 0 2
-1 1 0 0 1 1 1
-1 1 1 1 1 1 1
-1 1 1 1 1 1 1
-Напишите алгоритм определяющий наличие выхода из лабиринта и выводящий на экран  координаты точки выхода если таковые имеются.*/
-
 Stack<Tuple<int,int>> _path = new Stack<Tuple<int,int>>();
 
 int[,] labirynth1 = new int[,]
