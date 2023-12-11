49b73d5bfb68c32910081f9b147319430ec8c0c5
NoName <buybackbrain.genetic@gmail.com>
2023-12-11 16:52:07 +0300

    Home4 done

--- Sem4/Program.cs
+++ Sem4/Program.cs
@@ -1,11 +1,30 @@
 ﻿
+//Дан массив и число.
+//Найдите три числа в массиве сумма которых равна искомому числу.
+//Подсказка: если взять первое число в массиве,
+//можно ли найти в оставшейся его части два числа равных по сумме первому.
 
-string[] names = {"asd","csa","zxs","iqs","aws"};
+int[] arr = new []{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 
-var task = names.Where(x => x.StartsWith("a"));
+int target = 200;
+Console.WriteLine($"{target} - искомое число");
+bool flag = false;
+for (var i = 0; i < arr.Length; i++)
+{
+    for (var i1 = i+1; i1 < arr.Length; i1++)
+    {
+        for (var i2 = i1+1; i2 < arr.Length; i2++)
+        {
+            if (arr[i] + arr[i1] + arr[i2] == target)
+            {
+                flag = true;
+                Console.WriteLine($"{arr[i]:D2} + {arr[i1]:D2} + {arr[i2]:D2} = {target}");
+            }
+        }
+    }
+}
 
-Console.WriteLine(string.Join(" ",task));
-
-names[2] = "axs";
-
-Console.WriteLine(string.Join(" ",task));
+if (!flag)
+{
+    Console.WriteLine("Чисел нет");
+}
